70805347983b52e228670eeb12fdc03e02522b61
rafaelavlucas <rafaelalucas_91@hotmail.com>
2020-10-08 17:31:10 +0100

    upd: search

--- js/main.js
+++ js/main.js
@@ -3,113 +3,412 @@ window.onload = function (e) {
     // Populate Content
 
 
-    const ds = [{
+    const projects = [{
             id: 0,
-            client: "porto business school",
+            client: "Energy Digital Platform",
             thumb: "assets/thumbs/pbs.jpg",
             color: "#0b10b7",
-            url: "https://www.rafaelalucas.com/dailyui/",
-            user: "testweretsgf",
-            pass: "rgdrfgtf",
+            url: "https://google.com",
+            user: "f6ds_usr",
+            pass: "aRfvbGt67Hj",
             tools: [{
                     title: "Inspect",
                     name: "zeplin",
-                    link: "zeplin.com"
+                    link: "https://app.zeplin.io"
                 },
                 {
                     title: "Prototype",
                     name: "invision",
-                    link: "figma.com"
+                    link: "https://www.invisionapp.com"
                 },
                 {
                     title: "Wireframes",
                     name: "figma",
-                    link: "figma.com"
-                }
+                    link: "https://www.figma.com/"
+                },
             ]
         },
         {
             id: 1,
-            client: "Ageas Seguros",
+            client: "Portgás",
             thumb: "assets/thumbs/ageas.jpg",
             color: "#12A3BB",
-            url: "https://www.rafaelalucas.com/dailyui/",
-            user: "coco",
-            pass: "rgdrfgtf",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
             tools: [{
-
                     title: "Inspect",
                     name: "zeplin",
-                    link: "zeplin.com"
+                    link: "https://app.zeplin.io"
                 },
                 {
                     title: "Prototype",
                     name: "figma",
-                    link: "figma.com"
+                    link: "https://www.figma.com/"
                 },
                 {
                     title: "Wireframes",
                     name: "axure",
-                    link: "axure.com"
+                    link: "https://www.axure.com"
                 }
             ]
         },
+        {
+            id: 2,
+            client: "McDonald's",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 3,
+            client: "CUF",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 4,
+            client: "AICEP",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 5,
+            client: "Ferbar",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 6,
+            client: "Ageas Seguros",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 7,
+            client: "Ageas Portugal",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 8,
+            client: "Vodafone",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 9,
+            client: "Seguro Directo",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 10,
+            client: "Porto Business School",
+            thumb: "assets/thumbs/pbs.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 11,
+            client: "Porto Bay",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 12,
+            client: "Médis",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+        {
+            id: 13,
+            client: "Entreposto",
+            thumb: "assets/thumbs/ageas.jpg",
+            color: "#12A3BB",
+            url: "https://google.com",
+            user: "cenas_usr",
+            pass: "gtYhnJK78Sd",
+            tools: [{
+                    title: "Inspect",
+                    name: "zeplin",
+                    link: "https://app.zeplin.io"
+                },
+                {
+                    title: "Prototype",
+                    name: "figma",
+                    link: "https://www.figma.com/"
+                },
+                {
+                    title: "Wireframes",
+                    name: "axure",
+                    link: "https://www.axure.com"
+                }
+            ]
+        },
+
     ];
 
-    ds.forEach(function (el) {
+    projects.forEach(function (el) {
 
         let template = `
-        <div class="ds__item">
+        <div class="projects__item">
 
-        <figure class="ds__thumb"> <img src="${el.thumb}" alt=""></figure>
-        <span  class="ds__color" style="background-color:${el.color}"></span>
-        <h2 class="ds__title">${el.client}</h2>
+        <figure class="projects__thumb" style="background-color:${el.color}"> <img src="${el.thumb}" alt=""></figure>
+        <span  class="projects__color" style="background-color:${el.color}"></span>
+        <h2 class="projects__title">${el.client}</h2>
 
-        <div class="ds__details">
-            <div class="ds__detailsContent">
-                <input type="text">
-                <div class="ds__copyClipboard">
+        <div class="projects__details">
+            <div class="projects__detailsContent">
+                <input type="text" class="inputCopy">
+                <div class="projects__copyClipboard">
                     copied!
                 </div>
-                <div class="ds__info">
-                    <a class="ds__detail link" href="" target="_blank">Stage Link</a>
+                <div class="projects__info">
+                    <a class="projects__detail link" href="${el.url}" target="_blank">Stage Link</a>
                 </div>
-                <div class="ds__info">
-                    <p class="ds__detail--title user">User:</p>
-                    <p class="ds__detail--text">${el.user}</p>
+                <div class="projects__info">
+                    <p class="projects__detail--title user">User:</p>
+                    <p class="projects__detail--text">${el.user}</p>
 
                 </div>
-                <div class="ds__info">
-                    <p class="ds__detail--title pass">Password:</p>
-                    <p class="ds__detail--text">${el.pass}</p>
+                <div class="projects__info">
+                    <p class="projects__detail--title pass">Password:</p>
+                    <p class="projects__detail--text">${el.pass}</p>
                 </div>
             </div>
-            <div class="ds__tools">
+            <div class="projects__tools">
             </div>
         </div>
     </div>
     `;
 
-        document.querySelector(".ds").insertAdjacentHTML("beforeend", template);
+        document.querySelector(".projects").insertAdjacentHTML("beforeend", template);
 
         el.tools.forEach(function (tool) {
             let template2 = `
-            <a class="ds__toolLink href="${tool.link}">
-                <p class="ds__toolTitle">${tool.title}</p>
-                <p class="ds__toolName ${tool.name}">${tool.name}</p>
+            <a class="projects__toolLink" href="${tool.link}">
+                <p class="projects__toolTitle">${tool.title}</p>
+                <p class="projects__toolName ${tool.name}">${tool.name}</p>
             </a>`;
 
-            document.querySelectorAll(".ds__tools")[el.id].insertAdjacentHTML("beforeend", template2);
-
+            document.querySelectorAll(".projects__tools")[el.id].insertAdjacentHTML("beforeend", template2);
         })
+    });
 
-    })
 
 
     // Select and copy User and Password
 
-    const text = document.querySelectorAll('.ds__detail--text'),
-        copyClipboard = document.querySelectorAll('.ds__copyClipboard');
+    const text = document.querySelectorAll('.projects__detail--text'),
+        copyClipboard = document.querySelectorAll('.projects__copyClipboard');
 
     text.forEach(function (element) {
         element.addEventListener('click', selectText);
@@ -117,7 +416,7 @@ window.onload = function (e) {
 
 
     function selectText(event) {
-        input = document.querySelector('input');
+        input = document.querySelector('.inputCopy');
 
         input.value = event.currentTarget.innerHTML;
         input.select();
@@ -131,7 +430,7 @@ window.onload = function (e) {
         }, 1200);
 
         copyClipboard.forEach(function (el) {
-            event.currentTarget.closest('.ds__detailsContent').querySelector('.ds__copyClipboard').classList.add('show');
+            event.currentTarget.closest('.projects__detailsContent').querySelector('.projects__copyClipboard').classList.add('show');
 
             setTimeout(function () {
                 el.classList.remove('show');
@@ -140,7 +439,7 @@ window.onload = function (e) {
     }
 
     // Hover Items
-    const itemDs = document.querySelectorAll(".ds__item");
+    const itemDs = document.querySelectorAll(".projects__item");
     itemDs.forEach(function (el) {
         el.addEventListener("mouseenter", cenas)
     });
@@ -180,5 +479,37 @@ window.onload = function (e) {
             getTheme = localStorage.setItem('themeDark', false);
         }
     };
+
+
+    // Search
+
+    const searchInput = document.getElementById("search");
+
+    searchInput.addEventListener('keyup', function (event) {
+        const searchValue = event.target.value;
+
+        const filteredProjects = projects.filter(project => {
+            return project.client.includes(searchValue);
+        });
+
+
+        console.log(filteredProjects);
+
+
+        //showProjects(filteredProjects);
+
+
+    });
+
+
+    function clearList() {
+
+    }
+
+    function noResults() {
+
+    }
+
+
 
 }
